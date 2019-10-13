f69c2a01cd2926a96c0114a8c22d9414d1c603f7
VVlhw <vincent@stu.zsc.edu.cn>
2019-10-13 13:26:07 +0800

    重构代码，建立TankFrame类，并继承Frame类。重写paint方法，用系统给的Graphics参数绘制一个黑方块

---
+++ my_tank/src/lhw_tank/TankFrame.java
@@ -0,0 +1,38 @@
+package lhw_tank;
+
+import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class TankFrame extends Frame {
+	public TankFrame() {
+		setSize(800, 600);
+		setVisible(true);
+		setResizable(false); 								//无法改变 窗户大小,玩游戏嘛，肯定不能变
+		setTitle("LHW Tank War");
+		
+		//匿名类
+		addWindowListener(new WindowAdapter() {  			//WindowAdapter 实现了WindowListener接口
+			@Override
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+		
+		
+		
+		/*
+		 【JFrame与Frame的区别】
+		  1.JFrame是javax.swing.JFrame包中的类,Frame是java.awt.Frame包中的类
+		  2.关闭窗口的方式不同
+		  	JFrame.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
+		  	Frame要如上加监听事件
+		*/
+	}
+	
+	@Override
+	public void paint(Graphics g) {  						//接受 系统给的Graphics参数———画笔
+		g.fillRect(200, 200, 50, 50);						//用画笔绘制黑色方块
+	}
+}
