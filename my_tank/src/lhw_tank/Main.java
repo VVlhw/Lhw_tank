0ff6ef289472cf295f32a89f51ead75c987cc817
VVlhw <vincent@stu.zsc.edu.cn>
2019-10-17 21:30:43 +0800

    new出5个静态敌人坦克

--- my_tank/src/lhw_tank/Main.java
+++ my_tank/src/lhw_tank/Main.java
@@ -8,6 +8,10 @@ public class Main {
 
 	public static void main(String[] args) throws InterruptedException {
 		TankFrame tf = new TankFrame();
+		//初始化敌方坦克
+		for (int i = 0; i < 5; ++i) {
+			tf.tanks.add(new Tank(50 + i * 80, 200, Dir.DOWN, tf));
+		}
 		
 		while (true) {
 			Thread.sleep(50);     					 //每50毫秒 调用repaint函数，repaint调用paint函数（因为没画笔Graphics 这是系统传给paint的）
